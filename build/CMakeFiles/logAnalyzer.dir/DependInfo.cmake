8180ed2597c983895b5f168cf4b5ee817d8f291d
Bell Lee <jong@jbnu.ac.kr>
2025-05-01 15:48:18 +0900

    build directory is added.

---
+++ build/CMakeFiles/logAnalyzer.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/DATADRIVE1/Projects/BatteryGauge/LogAnayzer/logAnalyzer.c" "/DATADRIVE1/Projects/BatteryGauge/LogAnayzer/build/CMakeFiles/logAnalyzer.dir/logAnalyzer.c.o"
+  "/DATADRIVE1/Projects/BatteryGauge/LogAnayzer/opList.c" "/DATADRIVE1/Projects/BatteryGauge/LogAnayzer/build/CMakeFiles/logAnalyzer.dir/opList.c.o"
+  "/DATADRIVE1/Projects/BatteryGauge/LogAnayzer/print.c" "/DATADRIVE1/Projects/BatteryGauge/LogAnayzer/build/CMakeFiles/logAnalyzer.dir/print.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "IGRAPH_STATIC"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/DATADRIVE1/Projects/BatteryGauge/igraph/include"
+  "/DATADRIVE1/Projects/BatteryGauge/igraph/include/igraph"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/logAnalyzer.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/logAnalyzer.dir/logAnalyzer.c.o"
+  "CMakeFiles/logAnalyzer.dir/opList.c.o"
+  "CMakeFiles/logAnalyzer.dir/print.c.o"
+  "logAnalyzer"
+  "logAnalyzer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/logAnalyzer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
